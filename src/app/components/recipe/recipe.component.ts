d1914021a2002ec01a5c4b8f96eae5973931c448
Ru8ikcs <rjhansen@stud.ntnu.no>
2018-11-03 16:45:42 +0100

    view recipe and gracefully fail if instructions are missing

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -8,6 +8,13 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
+  ingredients =  [];
+  image: String;
+  title: String;
+  diets;
+  instructions;
+  readyInMinutes: number;
+  sourceUrl;
 
   constructor(private recipeService: RecipeService,
               private route: ActivatedRoute) {
@@ -17,8 +24,23 @@ export class RecipeComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.recipeService.getRecipe(params['id']).subscribe(res => {
         console.log(res);
+        console.log(res.extendedIngredients);
+        this.ingredients = res.extendedIngredients;
+        console.log(this.ingredients);
+        this.image = res.image;
+        this.title = res.title;
+        this.diets = res.diets;
+        this.sourceUrl = res.sourceUrl;
+        if (res.analyzedInstructions[0]) {
+          this.instructions = res.analyzedInstructions[0].steps;
+        }
+        this.readyInMinutes = res.readyInMinutes;
+        console.log(this.instructions);
       });
     });
+  }
+  viewSource() {
+    window.location.href = this.sourceUrl;
   }
 
 }
